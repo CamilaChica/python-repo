a49b1b20de11f978e8394c1db4eb2bb53af75425
Camila Chica <camilachica1991@gmail.com>
2025-03-16 17:09:17 -0400

    For loops

---
+++ app3.py
@@ -0,0 +1,23 @@
+#For Loops
+for item in ['Camila', 'Juan', 'Monica', 'Fabio']:
+    print(item)
+
+for item in [1, 2, 3, 4, 5]:
+    print(item)
+
+for item in range(10):
+    print(item)
+
+for item in range(5, 10):
+    print(item)
+
+for item in range(5, 10, 2):
+    print(item)
+
+#Excercise
+prices = [10, 20, 30]
+total = 0
+for price in prices:
+    total += price
+print(f"Total: {total}")
+
